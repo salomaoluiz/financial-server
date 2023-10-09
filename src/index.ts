b9d270a8a75723db03cd5448df88e2234d0b4507
salomao-neto <salomao.cruz.15@gmail.com>
2023-10-09 09:54:57 -0300

    feat: added post install

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,4 @@ import { swagger } from "@elysiajs/swagger";
 
 app.use(swagger()).listen(Bun.env?.PORT);
 
-console.log(`Server was started on port: http://localhost:${Bun.env.PORT}`);
+console.log(`Server was started on port: http://localhost:${Bun.env.PORT}/swagger`);
